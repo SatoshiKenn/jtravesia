af43737ab9371c7ff15d91f608cf949e0b5a1532
SatoshiKenn <guevara.kennette@gmail.com>
2022-04-11 14:06:21 -0400

    todos los niveles descripcion update

--- js/btnnivel.js
+++ js/btnnivel.js
@@ -1,7 +1,9 @@
 const cunamayor = document.querySelector("#btncuna");
 const cunacerrar = document.querySelector("#cunacerrar");
 const nivelmedio = document.querySelector("#btnmedio");
+const mediocerrar = document.querySelector("#nivelmedio");
 const transicion = document.querySelector("#btntransicion");
+const transicioncerrar = document.querySelector("#transicioncerrar");
 
 
 cunamayor.addEventListener("click", function() {
@@ -10,4 +12,20 @@ cunamayor.addEventListener("click", function() {
 
 cunacerrar.addEventListener("click", function() {
     document.querySelector("#cunamayor").style.display = "none";
+})
+
+nivelmedio.addEventListener("click", function() {
+    document.querySelector("#nivelmedio").style.display = "block";
+})
+
+mediocerrar.addEventListener("click", function() {
+    document.querySelector("#nivelmedio").style.display = "none";
+})
+
+transicion.addEventListener("click", function() {
+    document.querySelector("#transicion").style.display = "block";
+})
+
+transicioncerrar.addEventListener("click", function() {
+    document.querySelector("#transicion").style.display = "none";
 })
